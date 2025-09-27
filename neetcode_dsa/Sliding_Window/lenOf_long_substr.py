0fd7bc54fd872f64da9ee533abe63dccd2d448c3
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2025-09-27 15:22:38 +0530

    lenOf_long_Substr.py file added

---
+++ neetcode_dsa/Sliding_Window/lenOf_long_substr.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left, right, result, hashmap = 0, 0, 0, dict()
+        
+        while (right<len(s)):
+            if (s[right] in hashmap):
+                left = max(hashmap[s[right]]+1,left)
+            hashmap[s[right]] = right
+            result = max(result, ((right-left)+1))
+            right += 1
+        return result
+    
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.lengthOfLongestSubstring(s="zxyzxyz"))
