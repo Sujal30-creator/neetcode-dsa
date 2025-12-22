b28facbdecb3568875b8e1e949e4ef692af6b97e
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2025-12-22 17:15:56 +0530

    Nex xhnages

--- neetcode_dsa/Sliding_Window/long_repeating_char_replacement.py
+++ neetcode_dsa/Sliding_Window/long_repeating_char_replacement.py
@@ -1,3 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        pass
+        L,R=0,1
+        count = k
+        max_len = 1
+
+        while R<len(s):
+            if s[L] != s[R]:
+                if count == 0:
+                    max_len = max(max_len,R-L)
+                    L+=1
+                    R=L
+                    count = k
+                else:
+                    count -= 1
+            
+            R+=1
+
+        return max_len
+
+if __name__== "__main__":
+    sol = Solution()
+    print(sol.characterReplacement(s="ABAB",k=2))
