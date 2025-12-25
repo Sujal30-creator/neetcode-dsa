93398780c72a1e54a6c47978b7d12de802c69de8
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2025-12-25 16:01:40 +0530

    Leetcode q.no. 567  added

---
+++ neetcode_dsa/Sliding_Window/permutation_in_str.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        pass
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.checkInclusion(s1 = "abc", s2 = "lecabee"))
