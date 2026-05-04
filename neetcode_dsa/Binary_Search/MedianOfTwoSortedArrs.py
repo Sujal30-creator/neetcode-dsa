1462f4ed52c345a44266eeed05141a0556b2185f
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2026-05-04 20:53:44 +0530

    New work on 4th

---
+++ neetcode_dsa/Binary_Search/MedianOfTwoSortedArrs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        pass
+
+if __name__=="__main__":
+    sol = Solution()
+    nums1 = [1,3]
+    nums2 = [2,4]
+    print(sol.findMedianSortedArrays(nums1,nums2))
