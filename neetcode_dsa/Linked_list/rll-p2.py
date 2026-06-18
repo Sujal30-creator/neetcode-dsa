f34f58f2fca4fedb15ff265ae86c26d4a5e4e728
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2026-06-18 15:39:57 +0530

    LL rev done, now aiming for hard questions

---
+++ neetcode_dsa/Linked_list/rll-p2.py
@@ -0,0 +1,58 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def revList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        return prev
+
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        counter = 0
+        dummynode = ListNode(val=0, next=head)
+        curr = dummynode
+
+        if left == right:
+            return curr.next
+        
+        while curr:
+            if counter == left-1:
+                prev, start = curr, curr.next
+
+            elif counter == right:
+                end, after = curr, curr.next
+
+            curr = curr.next
+            counter += 1
+
+        end.next = None, None
+
+        new_head = self.revList(start)
+        
+        prev.next, start.next = new_head, after
+
+        return dummynode.next
+
+def build_linked_list(arr):
+    dummy = ListNode()
+    curr = dummy
+    for x in arr:
+        curr.next = ListNode(x)
+        curr = curr.next
+    return dummy.next
+
+if __name__=="__main__":    
+    sol = Solution()
+    head = build_linked_list([1,2,3,4,5])
+    print(sol.reverseBetween(head=head,left=2,right=4))
